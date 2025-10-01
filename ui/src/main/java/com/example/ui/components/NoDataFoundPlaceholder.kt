65bc6554fef4794127565bc126894b45d2f21e48
Abdallah Bakhet <abakhet2010@gmail.com>
2025-10-01 19:09:58 +0300

    feat: Center align NoDataFoundPlaceholder text

--- ui/src/main/java/com/example/ui/components/NoDataFoundPlaceholder.kt
+++ ui/src/main/java/com/example/ui/components/NoDataFoundPlaceholder.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -19,6 +20,7 @@ fun NoDataFoundPlaceholder(modifier: Modifier = Modifier) {
             style = AppTheme.textStyle.title.bold,
             color = AppTheme.color.primaryA,
             textAlign = TextAlign.Center,
+            modifier = Modifier.align(Alignment.Center),
         )
     }
 }
